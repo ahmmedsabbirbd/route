a66acfcc03c3973816dfedb13de8bc02ab09f96c
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-04-06 14:33:11 +0600

    using link component for navigate, image component for optimize image, head component for seo best,

--- pages/blog/blog1.js
+++ pages/blog/blog1.js
@@ -1,7 +1,29 @@
+import Head from "next/head";
+import Image from "next/image";
+
 const blog1 = () => {
     return (
         <div>
-            blog 1        
+            <Head>
+                <title>Blog 1</title>
+                <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width,initial-scale=1"/>
+                <meta name="description" content="website description"/>
+                <meta name="keyword" content="website description"/>
+                <meta name="authot" content="Sabbir ahmmed"/>
+                
+                <meta property="og:site_name" content="Sabbir Ahmmed"/>
+                <meta property="og:url" content="sabbir.com" />
+                <meta property="og:title" content="Sabbir Ahmmed" />
+                <meta property="og:description" content="website description" />
+                <meta property="og:image" content="./tk.jpg" />
+
+                <meta http-equiv="cache-control"  content="no-cache" />
+                <meta http-equiv="expires"  content="0" />
+                <meta http-equiv="pragma"  content="no-cache" />
+            </Head>
+            blog 1       
+            <Image src="/tk.jpg" width="120" height="120"></Image> 
         </div>
     );
 };
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 const index = () => {
     return (
         <div>
+            <Head>
+                <title>Blog Page</title>
+            </Head>
             nested blog
+            <Link href="/">Home</Link>
         </div>
     );
 };
